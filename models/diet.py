5b7b2c180b8e6cedfdb269b4e8e208ff50ae8690
sandro_mazzolla <jmazzolla94@gmail.com>
2024-03-15 21:57:20 -0300

    FEAT: alter description column to nullable

--- models/diet.py
+++ models/diet.py
@@ -4,6 +4,6 @@ from datetime import datetime
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(100), nullable=False, default="")
+    description = db.Column(db.String(100), nullable=True, default="")
     datetime = db.Column(db.DateTime(), nullable=False, default=datetime.now())
     is_diet = db.Column(db.Boolean(), nullable=False, default=True)
